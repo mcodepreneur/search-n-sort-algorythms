e402924530f697d7b1ad2a45fa382bdba56cf3e7
mcodepreneur <97573722+mcodepreneur@users.noreply.github.com>
2022-03-04 15:38:44 -0600

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class Sort here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Sort
 {
     public static void selectionSort(int[] arr)
